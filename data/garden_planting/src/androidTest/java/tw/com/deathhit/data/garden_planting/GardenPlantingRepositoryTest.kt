113c0a4b79ac0ed97cdcdff04d3ad1b21929cb8f
Deathhit <B9915034@gmail.com>
2023-11-19 18:01:00 +0800

    Implement the tests for :data:garden_planting.

---
+++ data/garden_planting/src/androidTest/java/tw/com/deathhit/data/garden_planting/GardenPlantingRepositoryTest.kt
@@ -0,0 +1,66 @@
+package tw.com.deathhit.data.garden_planting
+
+import androidx.paging.testing.asSnapshot
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import tw.com.deathhit.core.app_database.AppDatabase
+import tw.com.deathhit.core.app_database.entity.GardenPlantingEntity
+import tw.com.deathhit.data.garden_planting.config.generatePlantEntities
+import javax.inject.Inject
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
+class GardenPlantingRepositoryTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    internal lateinit var appDatabase: AppDatabase
+
+    private val gardenPlantingDao get() = appDatabase.gardenPlantingDao()
+    private val plantDao get() = appDatabase.plantDao()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun getGardenPlantingPagingDataFlow() = runTest {
+        //Given
+        val gardenPlantingRepository = GardenPlantingRepositoryImp(appDatabase = appDatabase)
+        val plantEntities = generatePlantEntities().sortedBy { it.plantName }
+
+        val gardenPlantingEntities = plantEntities.map {
+            GardenPlantingEntity(
+                plantId = it.plantId
+            )
+        }
+
+        gardenPlantingDao.upsert(gardenPlantingEntities)
+        plantDao.upsert(plantEntities)
+        advanceUntilIdle()
+
+        //When
+        val gardenPlantings = gardenPlantingRepository.getGardenPlantingPagingDataFlow()
+        val gardenPlantingsSnapshot = gardenPlantings.asSnapshot {
+            scrollTo(gardenPlantingEntities.size)
+        }
+
+        //Then
+        gardenPlantingsSnapshot.forEachIndexed { index, gardenPlanting ->
+            val gardenPlantingEntity = gardenPlantingEntities[index]
+            val plantEntity = plantEntities[index]
+
+            assert(gardenPlanting.plantId == gardenPlantingEntity.plantId)
+            assert(gardenPlanting.plantDate == gardenPlantingEntity.plantDate)
+            assert(gardenPlanting.plantName == plantEntity.plantName)
+        }
+    }
+}
