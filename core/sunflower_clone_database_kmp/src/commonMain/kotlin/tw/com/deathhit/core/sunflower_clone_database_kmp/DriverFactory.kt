e5be97ac8c2c297c93ce7fc8a160044768e1b4dd
Deathhit <B9915034@gmail.com>
2025-02-01 21:47:53 +0800

    Add SQLDelight primitive adapters to libs.versions.toml.

--- core/sunflower_clone_database_kmp/src/commonMain/kotlin/tw/com/deathhit/core/sunflower_clone_database_kmp/DriverFactory.kt
+++ core/sunflower_clone_database_kmp/src/commonMain/kotlin/tw/com/deathhit/core/sunflower_clone_database_kmp/DriverFactory.kt
@@ -1,7 +1,7 @@
 package tw.com.deathhit.core.sunflower_clone_database_kmp
 
 import app.cash.sqldelight.db.SqlDriver
-
+//todo apply primitive adapters
 expect class DriverFactory {
     fun createDriver(): SqlDriver
 }
